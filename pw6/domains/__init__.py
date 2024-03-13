defabb71d90931ce7bbfa6e32810a3cc57972e1d
anhchangngo <139603497+anhchangngo@users.noreply.github.com>
2024-03-13 16:49:39 +0700

    Remove files related to pw6

--- pw6/domains/__init__.py
+++
@@ -1,9 +0,0 @@
-# domains/__init__.py
-
-from .bcolors import bcolors
-from .Info import Info
-from .Student import Student
-from .Course import Course
-from .Class import Class
-
-__al__ = ["bcolors", "Info", "Student", "Course", "Class"]
